7b0f13822a67a529b87e7bc4f4b4a7bafd97ca46
Hákon Logi Bergsson <hakonlogibergsson@Hakons-Air.c.is>
2022-10-11 16:10:22 +0000

    hálfnaður með fyrri hlutann

--- src/s3/PointSet.java
+++ src/s3/PointSet.java
@@ -2,26 +2,36 @@ package s3;
 
 import edu.princeton.cs.algs4.Point2D;
 import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.RedBlackBST;
 
 public class PointSet {
+    private RedBlackBST pointset;
+    private int size;
+    RectHV rect;
 
     public PointSet() {
+        this.pointset = new RedBlackBST();
+        this.size = 0;
+        this.rect = new RectHV(0, 0, 0, 0); //Þetta á örugglega ekki að vera herna, bara nota þetta í ákveðnu falli
+
     }
 
     public boolean isEmpty() {
-        return true;
+        return this.size == 0;
     }
 
     public int size() {
-        return 1;
+        return size;
     }
 
-    public void insert() {
+    public void insert(Point2D p) {
+        this.pointset.put(p, 0);
+        this.size++;
 
     }
 
-    public boolean contains() {
-        return false;
+    public boolean contains(Point2D p) {
+        return this.pointset.contains(p);
     }
 
     public void draw() {
@@ -30,7 +40,7 @@ public class PointSet {
 
     // all points in the set that are inside the rectangle
     public Iterable<Point2D> range(RectHV rect) {
-        return new Point2D(3, 3);
+        return;
     }
 
     // a nearest neighbor in the set to p; null if set is empty
