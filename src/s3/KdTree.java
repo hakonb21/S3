2a6a295495b4ec4f664a31d2f514f172612a9dab
Hákon Logi Bergsson <hakonlogibergsson@Hakons-MacBook-Air.local>
2022-10-13 18:48:01 +0000

    á góðri leið með KdTree

--- src/s3/KdTree.java
+++ src/s3/KdTree.java
@@ -1,9 +1,143 @@
 package s3;
+/*************************************************************************
+ *************************************************************************/
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Out;
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+import java.util.Arrays;
 
 public class KdTree {
-    
-    public static void main(String[] args) {
+    private Node root;
+    private int size;
 
+    // construct an empty set of points
+    public class Node {
+        private Point2D key;
+        private RectHV rect;
+        private Node left;
+        private Node right;
+
+        public Node(Point2D key, RectHV rect) {
+            this.key = key;
+            this.rect = rect;
+            Node left;
+            Node right;
+        }
+    }
+
+    public KdTree() {
+        this.size = 0;
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return this.size == 0;
+    }
+
+    // number of points in the set
+    public int size() {
+        return this.size;
+    }
+
+    // add the point p to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        if (this.contains(p))
+            return;
+        this.root = insert_recursive();
+
+    }
+
+    public Node insert_recursive(Point2D p, Node node, boolean vertical) {
+        return new Node(new Point2D(2, 2), new Point2D(2, 2))
+    }
+
+
+    // does the set contain the point p?
+    public boolean contains(Point2D p) {
+        return false;
+    }
+
+    public boolean contains_recur_left() {
+        return true;
+    }
+
+    public boolean contains_recur_right() {
+        return false;
+    }
+
+    // draw all of the points to standard draw
+    public void draw() {
+
+    }
+
+    // all points in the set that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        return null;
+    }
+
+    // a nearest neighbor in the set to p; null if set is empty
+    public Point2D nearest(Point2D p) {
+        return p;
+    }
+
+    /*******************************************************************************
+     * Test client
+     ******************************************************************************/
+    public static void main(String[] args) {
+        In in = new In();
+        Out out = new Out();
+        int nrOfRecangles = in.readInt();
+        int nrOfPointsCont = in.readInt();
+        int nrOfPointsNear = in.readInt();
+        RectHV[] rectangles = new RectHV[nrOfRecangles];
+        Point2D[] pointsCont = new Point2D[nrOfPointsCont];
+        Point2D[] pointsNear = new Point2D[nrOfPointsNear];
+        for (int i = 0; i < nrOfRecangles; i++) {
+            rectangles[i] = new RectHV(in.readDouble(), in.readDouble(),
+                    in.readDouble(), in.readDouble());
+        }
+        for (int i = 0; i < nrOfPointsCont; i++) {
+            pointsCont[i] = new Point2D(in.readDouble(), in.readDouble());
+        }
+        for (int i = 0; i < nrOfPointsNear; i++) {
+            pointsNear[i] = new Point2D(in.readDouble(), in.readDouble());
+        }
+        KdTree set = new KdTree();
+        for (int i = 0; !in.isEmpty(); i++) {
+            double x = in.readDouble(), y = in.readDouble();
+            set.insert(new Point2D(x, y));
+        }
+        for (int i = 0; i < nrOfRecangles; i++) {
+            // Query on rectangle i, sort the result, and print
+            Iterable<Point2D> ptset = set.range(rectangles[i]);
+            int ptcount = 0;
+            for (Point2D p : ptset)
+                ptcount++;
+            Point2D[] ptarr = new Point2D[ptcount];
+            int j = 0;
+            for (Point2D p : ptset) {
+                ptarr[j] = p;
+                j++;
+            }
+            Arrays.sort(ptarr);
+            out.println("Inside rectangle " + (i + 1) + ":");
+            for (j = 0; j < ptcount; j++)
+                out.println(ptarr[j]);
+        }
+        out.println("Contain test:");
+        for (int i = 0; i < nrOfPointsCont; i++) {
+            out.println((i + 1) + ": " + set.contains(pointsCont[i]));
+        }
+
+        out.println("Nearest test:");
+        for (int i = 0; i < nrOfPointsNear; i++) {
+            out.println((i + 1) + ": " + set.nearest(pointsNear[i]));
+        }
+
+        out.println();
     }
 }
 
